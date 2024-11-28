44606c43842ab2eb75630cba3fc038adf12b8ce8
Lordimass <sam@tchrin.net>
2024-11-28 22:35:03 +0000

    Visuals
    Blogs will now show as raw text html rather than just being forced to navigate to blogs by their markdown files

---
+++ list_blogs.js
@@ -0,0 +1,21 @@
+const body = document.body
+var file_dirs = []
+
+fetch('blogs.json')
+  .then(response => response.json())
+  .then(data => {
+    file_dirs = data["blogs"]
+    show_blog_list(file_dirs)
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+function show_blog_list(blogs) {
+    blogs.forEach(blog => {
+        let a = document.createElement("a")
+        a.setAttribute("href", window.location.href + blog.slice(0, blog.length-3))
+        a.innerText = blog
+        body.appendChild(a)
+    });
+}
---
+++ load_blog.js
@@ -0,0 +1,59 @@
+let href = window.location.href
+let i = href.length-1
+
+
+// Fetch the list of valid blog pages
+var blogs = []
+fetch('blogs.json')
+  .then(response => response.json())
+  .then(data => {
+    blogs = data["blogs"]
+    load_blog(blogs)
+  })
+  .catch(error => {
+    console.error('Error fetching data:', error);
+  });
+
+function load_blog(blogs) {
+    // Getting subdirectory (The blog name)
+    for (i; i >= 0; i--) {
+        if (href[i] == "/") {
+            i++
+            break
+        }
+    }
+    let subdirectory = href.substring(i, href.length).replaceAll("%20", " ")
+
+    // If the blog exists, load it
+    if (is_in(blogs, subdirectory + ".md")) {
+        fetch('blog_markdowns/'+ subdirectory + ".md")
+        .then(response => response.text())
+        .then(data => {
+          show_blog(data)
+        })
+        .catch(error => {
+          console.error('Error fetching data:', error);
+        });
+    }
+}
+
+function is_in(list, element) {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i] === element) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+function show_blog(blog_raw) {
+    p = document.createElement("p")
+    p.innerText = blog_raw
+    document.body.appendChild(p)
+
+    a = document.createElement("a")
+    a.innerText = "<<< Return Home"
+    console.log(window.location.href.split("/"))
+    a.href = "http://" + window.location.href.split("/")[2]
+    document.body.appendChild(a)
+}
